5d16c78ac3d6d96006c9f55127ca2b73dd3c33e4
Sonu Thakur <136282638+sonuthakur03@users.noreply.github.com>
2025-05-02 22:45:03 +0545

    landing page

--- landingpage/js/projects.js
+++ landingpage/js/projects.js
@@ -4,65 +4,65 @@ export const projectsData = [
       path: "tic-tac-toe",
       description: "Classic game with responsive design",
       tags: ["Game", "Interactive"],
-      image: "images/ttt.png"
+      image: "landingpage/images/ttt.png"
     },
     {
       title: "Weather Dashboard",
       path: "weather-dashboard",
       description: "Real-time weather forecasts",
       tags: ["API", "Weather"],
-      image: "images/weatherdash.png"
+      image: "landingpage/images/weatherdash.png"
     },
     {
       title: "Text Reader",
       path: "text-read-speak/dist",
       description: "Text-to-speech application",
       tags: ["Web Speech API", "Accessibility"],
-      image: "images/textreader.png"
+      image: "landingpage/images/textreader.png"
     },
     {
       title: "To-Do App",
       path: "todo-app",
       description: "Task management application",
       tags: ["CRUD", "Productivity"],
-      image: "images/todo.png"
+      image: "landingpage/images/todo.png"
     },
     {
       title: "Calculator",
       path: "calculator",
       description: "Functional calculator with themes",
       tags: ["Math", "Utility"],
-      image: "images/calculator.png"
+      image: "landingpage/images/calculator.png"
     },
     {
         title: "Rock Paper Scissors",
         path: "rock-paper-scissors",
         description: "Classic game with a twist",
         tags: ["Game", "Interactive"],
-        image: "images/rps.png"
+        image: "landingpage/images/rps.png"
     },{
         title: "language-translator",
         path: "language-translator",
         description: "Language translation application",
         tags: ["API", "Translation"],
-        image: "images/langtrans.png"
+        image: "landingpage/images/langtrans.png"
     },{
         title: "Hangman",
         path: "hangman-game",
         description: "Word guessing game",
         tags: ["Game", "Interactive"],
-        image: "images/hangman.png"
+        image: "landingpage/images/hangman.png"
     },{
         title: "Slider Image Gallery",
         path: "draggable-slider",
         description: "Image gallery with draggable slider",
         tags: ["Gallery", "Interactive"],
-        image: "images/slider.png"
+        image: "landingpage/images/slider.png"
     },{
         title: "Stop Watch",
         path: "stopTimer",
         description: "Simple stopwatch application",
         tags: ["Timer", "Utility"],
-        image: "images/stopwatch.png"
+        image: "landingpage/images/stopwatch.png"
     },   
   ];
